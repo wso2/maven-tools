81402660389f858e05b3681244a650334bb451bc
gayank <gayank@wso2.com>
2015-06-29 20:35:40 +0530

    Package prepare plugin modification

--- wso2-maven-package-prepare-plugin/src/main/java/org/wso2/maven/pckg/prepare/PackagePrepareDefaultScopeMojo.java
+++ wso2-maven-package-prepare-plugin/src/main/java/org/wso2/maven/pckg/prepare/PackagePrepareDefaultScopeMojo.java
@@ -14,17 +14,12 @@
  * limitations under the License.
  */
 
-package org.wso2.maven.packageprepare;
+package org.wso2.maven.pckg.prepare;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import javax.xml.stream.FactoryConfigurationError;
-
-import org.apache.maven.artifact.Artifact;
 import org.apache.maven.model.Dependency;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -33,21 +28,23 @@ import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.project.MavenProject;
 import org.codehaus.plexus.components.interactivity.Prompter;
 import org.codehaus.plexus.components.interactivity.PrompterException;
-import org.wso2.developerstudio.eclipse.utils.file.FileUtils;
 import org.wso2.maven.core.utils.MavenConstants;
 import org.wso2.maven.core.utils.MavenUtils;
-import org.wso2.maven.packageprepare.util.PackagePrepareUtils;
 
 /**
- * This Maven Mojo is used to change all dependencies to system scope.
+ * This Maven Mojo is used to change all dependencies to default scope.
  * 
- * @goal system-scope
+ * @goal default-scope
  * @requiresProject
  * @aggregator
  * @since 1.0.0
  * 
  */
-public class PackagePrepareSystemScopeMojo extends AbstractMojo {
+public class PackagePrepareDefaultScopeMojo extends AbstractMojo {
+	private static final String USER_CONSENT_PROMPTER = "Continue? [Y/n]";
+	private static final String USER_CONSENT_YES = "y";
+	private static final String USER_CONSENT_NO = "n";
+	private static final String USER_CONSENT_DEFAULT = "";
 
 	private final Log log = getLog();
 
@@ -62,6 +59,11 @@ public class PackagePrepareSystemScopeMojo extends AbstractMojo {
 	private boolean dryRun;
 
 	/**
+	 * @parameter expression="${updateDependencies}" default-value="false"
+	 */
+	private boolean updateDependencies;
+
+	/**
 	 * Prompter component for user input
 	 * 
 	 * @component
@@ -69,9 +71,11 @@ public class PackagePrepareSystemScopeMojo extends AbstractMojo {
 	private Prompter prompter;
 
 	private List<MavenProject> cappMavenProjects;
-	private Map<String, String> dependencySystemPathMap;
+	private boolean isDebugEnabled;
 
 	public void execute() throws MojoExecutionException, MojoFailureException {
+		isDebugEnabled = log.isDebugEnabled();
+
 		log.info("MavenPackagePrepare plugin execution started");
 		if (dryRun) {
 			log.warn(" **** wso2-maven-package-prepare-plugin does not support dryRun mode **** ");
@@ -86,13 +90,12 @@ public class PackagePrepareSystemScopeMojo extends AbstractMojo {
 			// Aggregate dependencies from sub modules only if this is a maven
 			// multi-module project
 			List<MavenProject> mavenProjects = getMavenProjects(modules);
-			aggregateDependencies(mavenProjects);
 
 			// Update pom files of capp projects only
 			filterAllCappProjects(mavenProjects);
-			if (!updateAllDependencies()) {
-				log.error("Failed to update POM");
-				throw new MojoFailureException("Failed to update POM");
+			if (!updateCappDependencies()) {
+				log.error("Plugin execution terminated");
+				throw new MojoFailureException("Plugin execution terminated");
 			}
 		} else {
 			log.error("\"" + project.getBasedir() + "\"" + " does not contain a Maven Multi-Module project. "
@@ -104,9 +107,13 @@ public class PackagePrepareSystemScopeMojo extends AbstractMojo {
 	}
 
 	private List<MavenProject> getMavenProjects(List<String> modules) {
-		List<MavenProject> projectList = new ArrayList<MavenProject>();
+		List<MavenProject> projectList = new ArrayList<>();
 
 		for (String module : modules) {
+			if (isDebugEnabled) {
+				log.debug("Reading module: " + module);
+			}
+
 			StringBuilder modulePomFilePath = new StringBuilder();
 			modulePomFilePath.append(module);
 			modulePomFilePath.append(File.separator);
@@ -130,102 +137,73 @@ public class PackagePrepareSystemScopeMojo extends AbstractMojo {
 		return projectList;
 	}
 
-	private void aggregateDependencies(List<MavenProject> mavenProjects) {
-		dependencySystemPathMap = new HashMap<String, String>();
-
-		for (MavenProject mavenProject : mavenProjects) {
-			String packaging = mavenProject.getPackaging();
-			// CAPP projects are ignored.
-			if (packaging == null || !packaging.equals(MavenConstants.CAPP_PACKAGING)) {
-				try {
-					dependencySystemPathMap.putAll(PackagePrepareUtils.getDependencyData(mavenProject));
-				} catch (FactoryConfigurationError | Exception e) {
-					log.error("Failed to retrieve dependencies from project: " + mavenProject.getGroupId() + ":"
-							+ mavenProject.getArtifactId() + ":" + mavenProject.getVersion(), e);
-				}
-			}
-		}
-	}
-
 	private void filterAllCappProjects(List<MavenProject> mavenProjects) {
-		cappMavenProjects = new ArrayList<MavenProject>();
+		cappMavenProjects = new ArrayList<>();
 
 		for (MavenProject mavenProject : mavenProjects) {
 			String packaging = mavenProject.getPackaging();
 			if (packaging != null && packaging.equals(MavenConstants.CAPP_PACKAGING)) {
-				log.debug("Identified the composite application project: " + mavenProject.getGroupId() + ":"
-						+ mavenProject.getArtifactId() + ":" + mavenProject.getVersion());
+				if (isDebugEnabled) {
+					log.debug("Identified the composite application project: " + mavenProject.getGroupId() + ":"
+							+ mavenProject.getArtifactId() + ":" + mavenProject.getVersion());
+				}
 				cappMavenProjects.add(mavenProject);
 			}
 		}
 	}
 
-	private boolean updateAllDependencies() {
+	private boolean updateCappDependencies() {
 		for (MavenProject cappMavenProject : cappMavenProjects) {
 			log.info("About to update: " + cappMavenProject.getFile().getAbsolutePath());
-			log.info("All dependencies will be converted to system scope");
+			log.warn("All dependencies will be converted to default scope");
 
-			// Ask user consent to proceed
-			String userInput = "";
-			do {
-				try {
-					userInput = prompter.prompt("Continue? (Y/n)");
-				} catch (PrompterException e) {
-					log.error("Failed to get user input", e);
+			// If updateDependencies parameter is provided in command line
+			if (updateDependencies) {
+				if (!updatePomFile(cappMavenProject)) {
 					return false;
 				}
-			} while (!("y".equalsIgnoreCase(userInput) || "n".equalsIgnoreCase(userInput)));
-
-			if ("n".equalsIgnoreCase(userInput)) {
-				log.info("Plugin execution terminated");
-				return false;
 			} else {
-				@SuppressWarnings("unchecked")
-				List<Dependency> dependencies = cappMavenProject.getDependencies();
-				for (Dependency dependency : dependencies) {
-					dependency.setScope(Artifact.SCOPE_SYSTEM);
-					String absoluteSystemPath = resolveDependencySystemPath(dependency);
-					if (!absoluteSystemPath.equals("")) {
-						// Derive system path relative to this CAPP project
-						StringBuilder systemPathBuilder = new StringBuilder();
-						systemPathBuilder.append(MavenConstants.MAVEN_BASE_DIR_PREFIX);
-						systemPathBuilder.append(File.separator);
-						systemPathBuilder.append(FileUtils.getRelativePath(new File(cappMavenProject.getBasedir()
-								.toString()), new File(absoluteSystemPath)));
+				// Ask user consent to proceed
+				String userInput = "";
+				do {
+					try {
+						userInput = prompter.prompt(USER_CONSENT_PROMPTER);
+					} catch (PrompterException e) {
+						log.error("Failed to get user input while updating dependencies", e);
+						return false;
+					}
+				} while (!(USER_CONSENT_DEFAULT.equalsIgnoreCase(userInput)
+						|| USER_CONSENT_YES.equalsIgnoreCase(userInput) || USER_CONSENT_NO.equalsIgnoreCase(userInput)));
 
-						dependency.setSystemPath(systemPathBuilder.toString());
-					} else {
-						// Reason is logged in a previous step
+				// Empty input is considered as default (Y)
+				if (USER_CONSENT_NO.equalsIgnoreCase(userInput)) {
+					log.info("Skipped updating file: " + cappMavenProject.getFile().getAbsolutePath());
+				} else {
+					if (!updatePomFile(cappMavenProject)) {
 						return false;
 					}
 				}
-				try {
-					MavenUtils.saveMavenProject(cappMavenProject, cappMavenProject.getFile());
-				} catch (Exception e) {
-					log.error("Failed to save the pom file", e);
-					return false;
-				}
-				log.info("All dependencies were converted into system scope");
 			}
 		}
 		return true;
 	}
 
-	private String resolveDependencySystemPath(Dependency dependency) {
-		StringBuilder dependencyStringBuilder = new StringBuilder();
-		dependencyStringBuilder.append(dependency.getGroupId());
-		dependencyStringBuilder.append(":");
-		dependencyStringBuilder.append(dependency.getArtifactId());
-		dependencyStringBuilder.append(":");
-		dependencyStringBuilder.append(dependency.getVersion());
-		String dependencyString = dependencyStringBuilder.toString();
-
-		String systemPath = dependencySystemPathMap.get(dependencyString);
-		if (systemPath != null) {
-			return systemPath;
-		} else {
-			log.error("Could not resolve dependency: " + dependencyString + " from any sub module");
-			return "";
+	private boolean updatePomFile(MavenProject cappMavenProject) {
+		@SuppressWarnings("unchecked")
+		List<Dependency> dependencies = cappMavenProject.getDependencies();
+		for (Dependency dependency : dependencies) {
+			// Set default scope
+			dependency.setScope(null);
+			dependency.setSystemPath(null);
 		}
+
+		try {
+			MavenUtils.saveMavenProject(cappMavenProject, cappMavenProject.getFile());
+		} catch (Exception e) {
+			log.error("Failed to save the pom file", e);
+			return false;
+		}
+		log.info("All dependencies were converted to default scope");
+		return true;
 	}
 }
