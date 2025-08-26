9686194e7e524e91253c88eddbd7514caabcbbc9
Chinthaka Jayatilake <37581983+ChinthakaJ98@users.noreply.github.com>
2025-08-26 11:57:57 +0530

    Add support to build from local maven

--- vscode-car-plugin/src/main/java/org/wso2/maven/MavenUtils.java
+++ vscode-car-plugin/src/main/java/org/wso2/maven/MavenUtils.java
@@ -23,14 +23,23 @@ import org.apache.maven.project.MavenProject;
 import org.apache.maven.shared.invoker.InvocationOutputHandler;
 import org.apache.maven.shared.invoker.Invoker;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class MavenUtils {
+
+    private static final Logger LOGGER = Logger.getLogger(MavenUtils.class.getName());
 
     /**
      * Retrieves the Maven home directory.
@@ -85,7 +94,7 @@ public class MavenUtils {
 
         File mvnwFile = new File(projectPath, System.getProperty("os.name").toLowerCase().
                 contains("win") ? "mvnw.cmd" : "mvnw");
-        boolean useMavenWrapper = mvnwFile.exists() && mvnwFile.canExecute();
+        boolean useMavenWrapper = mvnwFile.exists() && mvnwFile.canExecute() && !useLocalMaven(projectPath);
 
         if (useMavenWrapper) {
             invoker.setMavenExecutable(mvnwFile);
@@ -183,5 +192,24 @@ public class MavenUtils {
             writer.write("</project>\n");
         }
         return tempPom;
+    }
+
+    public static boolean useLocalMaven(String projectPath) {
+        try {
+            File settingsFile = Paths.get(projectPath, ".vscode", "settings.json").toFile();
+            if (!settingsFile.exists()) {
+                return false;
+            }
+
+            String content = new String(Files.readAllBytes(settingsFile.toPath()), StandardCharsets.UTF_8);
+            JsonObject jsonObject = new JsonParser().parse(content).getAsJsonObject();
+
+            if (jsonObject.has(Constants.LOCAL_MVN_SETTING)) {
+                return jsonObject.get(Constants.LOCAL_MVN_SETTING).getAsBoolean();
+            }
+        } catch (Exception e) {
+            LOGGER.log(Level.WARNING, "Error occurred while reading project settings file", e);
+        }
+        return false;
     }
 }
